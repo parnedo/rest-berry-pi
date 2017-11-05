3f3a6fb6b6485e0d48e43eb1fafa5444b3b95d74
Pedro Arnedo <parnedo@gmail.com>
2017-11-05 21:05:46 +0000

    Added support for sensor AM2302

--- src/raspberry.py
+++ src/raspberry.py
@@ -1,10 +1,11 @@
 from model import models
-
+import Adafruit_DHT
 
 class Raspberry:
     def __init__(self):
         self._model = None
         self._setup = None
+        self._extra_AM2302=[]
         import os.path
         if os.path.isfile('/sys/firmware/devicetree/base/model'):
            self._model = models[open('/sys/firmware/devicetree/base/model','r').read().replace('\x00','')]
@@ -19,7 +20,12 @@ class Raspberry:
         self._setup = config
         self._model.setupBoardMode(config['board_mode'])
         for pin in config['pins']:
-           self._model.setupPin(int(pin['pin']), pin['es'], pin['initial'])
+           if "extra" not in pin.keys():
+             self._model.setupPin(int(pin['pin']), pin['es'], pin['initial'])
+           else:
+             print "Setting pin[",pin['pin'],"] as [",pin['extra'],"]"
+             if pin["extra"] == "AM2302":
+                 self._extra_AM2302.append(int(pin["pin"]))
         
     def getSetup(self):
         if self._setup:
@@ -43,7 +49,13 @@ class Raspberry:
         for pin in self._setup ['pins']:
             single_pin = {}
             single_pin['pin'] = pin['pin']
-            single_pin['value'] = self._model.getPinValue(int(pin['pin']))
+            if pin['pin'] in self._extra_AM2302:
+                single_pin['extra'] = "AM2302"
+                humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, pin['pin'])
+                single_pin['humidity'] = humidity
+                single_pin['temperature'] = temperature
+            else:
+                single_pin['value'] = self._model.getPinValue(int(pin['pin']))
             output['pins'].append(single_pin)
         return output 
 
